b8693c12d1f4fe535c721964210bf6abf9d829f5
Younes Tahiri <you@example.com>
2023-08-13 13:33:14 -0400

    added sech input that searches for student by name

--- src/components/StudentList/StudentList.js
+++ src/components/StudentList/StudentList.js
@@ -1,10 +1,58 @@
+import { useState } from "react";
 import StudentCard from "../StudentCard/StudentCard";
 import "./StudentList.css";
 
 const StudentList = ({ studentData }) => {
-  // creates a student card for each student.
-  const students = studentData.map((student) => <StudentCard student={student} key={student.id} />);
-  return <div className="StudentList">{students}</div>;
+  // when I type the input, I should see student by name
+  const [searchInput, setSearchInput] = useState("");
+
+  // handles the user input and sets the value to the searchInput state.
+  const handleChange = (event) => {
+    setSearchInput(event.target.value);
+  };
+
+  // bode of function - filter data here
+  // create a var to hold the filtered data
+  let dataToDisplay = studentData;
+
+  // if searchInput is truthy
+  // reassign dataToDisplay with filtered data
+  if (searchInput) {
+    dataToDisplay = studentData.filter((student) => {
+      const name = searchInput.toLowerCase();
+      const { firstName, lastName } = student;
+      const fullName = `${firstName} ${lastName}`.toLowerCase();
+
+      return fullName.includes(name);
+    });
+  }
+
+  const renderContent = () => {
+    // if dataToDisplay === 0 display 'No students found' message else display list of students.
+    let contentClassName = "StudentList__content";
+    if (dataToDisplay.length) {
+      return (
+        <div className={contentClassName}>
+          {dataToDisplay.map((student) => (
+            <StudentCard student={student} key={student.id} />
+          ))}
+          ;
+        </div>
+      );
+    } else {
+      contentClassName += " StudentList__content--center";
+      return <div className={contentClassName}>{`No Students Found By That Name ${searchInput}`}</div>;
+    }
+  };
+
+  return (
+    <div className="StudentList">
+      <div className="StudentList__input">
+        <input value={searchInput} type="text" placeholder="Search By Name" onChange={handleChange} />
+      </div>
+      {renderContent()}
+    </div>
+  );
 };
 
 export default StudentList;
